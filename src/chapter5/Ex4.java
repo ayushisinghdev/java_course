8e3a2723de933dce7c0e48d11fdee902a58ab219
ayushidma@gmail.com <ayushidma@gmail.com>
2025-02-24 22:49:02 +0000

    New file

--- src/chapter5/Ex4.java
+++ src/chapter5/Ex4.java
@@ -1,4 +1,61 @@
 package chapter5;
 
+import java.util.Scanner;
+
+/*
+Final total of someone's cell phone bill
+operator will i/p plan fee & no of overage min
+charge 0.25/min over the plan + 15% tax on sub total
+methods - calculate tax, overage fees and final total
+print itemized bill
+ */
 public class Ex4 {
+    static double overageCharge = 0.25;
+    static double tax = 15;
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String args[]){
+
+        double planFee = planFee();
+        double overageMin = overageMin();
+        scanner.close();
+        double overageFees = calculateOverageFees(overageMin);
+        double subTotal = getSubTotal(planFee,overageFees);
+        double taxToBePaid = calculateTax(subTotal);
+        double finalBill = calculateFinalBill(subTotal,taxToBePaid);
+        printItemizedBill(planFee,overageFees,subTotal,taxToBePaid,finalBill);
+    }
+
+    public static double planFee(){
+        System.out.println("Enter the plan fee:");
+        double planFee = scanner.nextDouble();
+        return planFee;
+    }
+    public static double overageMin(){
+        System.out.println("Enter the overage min:");
+        double overageMin = scanner.nextDouble();
+        return overageMin;
+    }
+    public static double calculateOverageFees(double overageMin){
+        double overageFees = overageMin*overageCharge;
+        return overageFees;
+    }
+    public static double getSubTotal(double planFee,double overageFees){
+        double subTotal = planFee + overageFees;
+        return subTotal;
+    }
+    public static double calculateTax(double subTotal){
+        double taxToBePaid = ((tax/100)*subTotal);
+        return taxToBePaid;
+    }
+    public static double calculateFinalBill(double subTotal,double taxToBePaid){
+        double finalBill = taxToBePaid + subTotal;
+        return finalBill;
+    }
+    public static void printItemizedBill(double planFee, double overageFees, double subTotal, double taxToBePaid, double finalBill){
+        System.out.println("Your plan fee is " + planFee);
+        System.out.println("Your overage fee is " + overageFees);
+        System.out.println("Your sub total is " + subTotal);
+        System.out.println("Your tax is " + taxToBePaid);
+        System.out.println("Your final bill is " + finalBill);
+    }
 }
