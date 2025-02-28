352f4858a1fa61e394d12f5d7704a5054effcc98
ayushidma@gmail.com <ayushidma@gmail.com>
2025-02-28 22:51:16 +0000

    New file

---
+++ src/Coin.java
@@ -0,0 +1,27 @@
+import com.sun.source.tree.BreakTree;
+
+import java.util.Random;
+
+public class Coin {
+
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    private String side;
+    public static String HEADS = "Heads";
+    public static String TAILS = "Tails";
+
+    public String flip(){
+        if (new Random().nextBoolean()){
+            setSide(HEADS);
+        }else
+            setSide(TAILS);
+        return getSide();
+    }
+
+}
---
+++ src/CoinTossGame.java
@@ -0,0 +1,10 @@
+public class CoinTossGame {
+
+    public static void main(String args[]){
+
+        Player player1 = new Player("Ayushi");
+        Player player2 = new Player("Veena");
+
+        player1.flip();
+    }
+}
